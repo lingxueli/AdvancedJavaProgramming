a8d31452e126ad35d0e42abf2a63e1de621361a7
lingxueli <ll2871@columbia.edu>
2019-05-28 20:40:04 -0600

    varargs

---
+++ Varargs.java
@@ -0,0 +1,24 @@
+public class Varargs {
+    
+    public static void main(String[] args) {
+        String imem1 = "Apples";        
+        String item2 = "Oranges";
+        String item3 = "Pears";
+        printShoppingList(imem1, item2, item3);
+        printShoppingList("Bread", "Milk", "Eggs", "Bananas");
+    }
+
+    
+
+    // Because of varargs, the multiple parameters are migrated into an arraylist
+    private static void printShoppingList(String... items) {
+        System.out.println("SHOPPING LIST");
+        for (int i = 0; i < items.length; i++) {
+            System.out.println(i + 1 + ": " + items[i]);
+        }
+        System.out.println();
+
+    }
+    
+    
+}
